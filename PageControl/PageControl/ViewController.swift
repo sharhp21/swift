e2af97e2d794e3afd2db18262e334adfb87c77ed
sharhp21 <sharhp21@gmail.com>
2023-03-15 14:29:46 +0900

    PageControl_20230315

---
+++ PageControl/PageControl/ViewController.swift
@@ -0,0 +1,31 @@
+//
+//  ViewController.swift
+//  PageControl
+//
+//  Created by ssg on 2023/03/15.
+//
+
+import UIKit
+
+var images = ["01.png", "02.png", "03.png", "04.png", "05.png", "06.png"]
+
+class ViewController: UIViewController {
+    @IBOutlet var pageControl: UIPageControl!
+    @IBOutlet var imgView: UIImageView!
+    
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view.
+        pageControl.numberOfPages = images.count
+        pageControl.currentPage = 0
+        pageControl.pageIndicatorTintColor = UIColor.white
+        pageControl.currentPageIndicatorTintColor = UIColor.red
+        imgView.image = UIImage(named: images[0])
+    }
+
+    @IBAction func pageChange(_ sender: UIPageControl) {
+        imgView.image = UIImage(named: images[pageControl.currentPage])
+    }
+    
+}
+
